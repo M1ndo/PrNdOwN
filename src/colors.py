8c8693f1919a546e9cc815f7dd4f9cd2b43090cd
r2dr0dn <doophack@gmail.com>
2020-04-09 21:14:43 +0100

    New Update

---
+++ src/colors.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# Created By Ybenel(r2dr0dn)
+from random import randint
+list = ["\033[1;33m","\033[1;34m","\033[1;30m","\033[1;36m","\033[1;31m","\033[35m","\033[95m","\033[96m","\033[39m","\033[38;5;82m","\033[38;5;198m","\033[38;5;208m","\033[38;5;167m","\033[38;5;91m","\033[38;5;210m","\033[38;5;165m","\033[38;5;49m","\033[38;5;160m","\033[38;5;51m","\033[38;5;13m","\033[38;5;162m","\033[38;5;203m","\033[38;5;113m","\033[38;5;14m"]
+class get_colors():
+    def randomize():
+        return list[randint(0,23)]
+    def randomize1():
+        return list[randint(0,23)]
+    def randomize2():
+        return list[randint(0,23)]
+    def randomize3():
+        return list[randint(0,23)]      
+    def yellow():
+        return list[0]
+    def cyan():
+        return list[3]    
+    def red():
+        return list[4]
+    def white():
+        return list[8]
+    def green():
+        return list[9]
+    def magento():
+        return list[6]
+    def sharp_green():
+        return list[22]
+    def bright_megento():
+        return list[14]
+    def pink():
+        return list[10]
+    def sharp_megento():
+        return list[12]
+    def orange():
+        return list[11]
+    def sharp_orange():
+        return list[21]          
+        
+        
